76ad2695104473e63f08d8ae80e1a9aa94ae1326
tehcnicallyAustin <Technicallyaustin@gmail.com>
2023-02-17 12:34:33 -0700

    creating base logic for game

---
+++ src/api.js
@@ -0,0 +1,5 @@
+const ticTacToe = () => {
+    const header = () => {} // creates the header element. creates a nav element. creates a page title.
+    const main = () => {} // creates a main element. creates a section element, creates an aside element. creates a start button.
+    const footer = () => {} // creates the footer. creates footer text
+}
